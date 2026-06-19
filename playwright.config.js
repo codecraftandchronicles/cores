f3688d9facd7d58f46a28c4a671257a4370292b4
Bruno Amaral <Bruno.ramaral@gmail.com>
2026-06-19 12:16:57 +0100

    Update playwright.config.js
    removing webkit from the tests

--- playwright.config.js
+++ playwright.config.js
@@ -59,10 +59,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    //{
+    //  name: 'webkit',
+    //  use: { ...devices['Desktop Safari'] },
+    //},
 
     /* Test against mobile viewports. */
     // {
